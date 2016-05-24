f29f5265a763e103cf0e94b6b91f740393b1c978
Enrique <enrique.turcott@cimav.edu.mx>
2016-05-24 14:46:16 -0600

    Add shuffle script

---
+++ shuffle.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+=begin
+   This script shuffle songs list from spotify ('Copy HTML Link' option) 
+   Paste HTML Link songs in spotity file and run:
+   user@mypc$shuffle.rb spotify
+=end
+
+
+archivo = ARGV[0]
+
+File.open(archivo,'r') do |s|
+  while line = s.gets
+    songs = line.split(" ")
+  end
+
+  #songs.each do |s|
+  #  puts s
+  #end
+
+  s_songs = songs.shuffle
+  #puts "----------------------------------------"
+  s_songs.each do |s|
+    puts s
+  end
+end
