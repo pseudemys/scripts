2f6dfb16a1e1801c744bf792aa8e3d88b18f7eff
Enrique <enrique.turcott@cimav.edu.mx>
2016-06-01 11:24:14 -0600

    Automatic spotify file load

--- shuffle.rb
+++ shuffle.rb
@@ -2,27 +2,18 @@
 # encoding: UTF-8
 
 =begin
-   A little change...
-   Other test changes ...
    This script shuffle songs list from spotify ('Copy HTML Link' option) 
-   Paste HTML Link songs in spotity file and run:
-   user@mypc$shuffle.rb spotify
+   Paste HTML Link songs in spotity file and run
 =end
 
-
-archivo = ARGV[0]
+archivo = "spotify"
 
 File.open(archivo,'r') do |s|
   while line = s.gets
     songs = line.split(" ")
   end
 
-  #songs.each do |s|
-  #  puts s
-  #end
-
   s_songs = songs.shuffle
-  #puts "----------------------------------------"
   s_songs.each do |s|
     puts s
   end
